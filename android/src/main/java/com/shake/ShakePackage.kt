9f689d6e8d4af02e8dd7fd3883b6a48e954edc27
Quan Pham <doraemonfanclub@gmail.com>
2025-08-03 21:25:19 +0700

    feat(android): disable daemon spawning

--- android/src/main/java/com/shake/ShakePackage.kt
+++ android/src/main/java/com/shake/ShakePackage.kt
@@ -1,13 +1,13 @@
 package com.shake
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.module.model.ReactModuleInfo
 import java.util.HashMap
 
-class ShakePackage : TurboReactPackage() {
+class ShakePackage : BaseReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return if (name == ShakeModule.NAME) {
       ShakeModule(reactContext)
