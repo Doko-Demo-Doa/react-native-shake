bca3c9722f7792c897f587ca240b5ebe23a022e7
Quan Pham <doraemonfanclub@gmail.com>
2026-04-04 22:25:29 +0700

    fix: react-native 0.79 workaround fix (#124)
    * fix: remove getTurboModule method for RN 0.79 compatibility
    
    * fix: refactor ShakeExample project structure and update dependencies
    
    * fix: update expo
    
    * fix: update CI workflow for improved caching and prebuild steps
    
    * fix: bump version to 6.8.4-beta.0 in package.json

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const pkg = require('../package.json');
 
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    },
-  },
   dependencies: {
     [pkg.name]: {
       root: path.join(__dirname, '..'),
